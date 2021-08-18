11ff3ce9e739230491aaecaea338b748f9c8a91b
sjswjeon <sangwon.jeon.uk@gmail.com>
2021-08-19 00:07:40 +0900

    - register.html 회원가입 페이지 생성
    - signin.html 로그인 페이지 생성

---
+++ src/main/java/com/example/boardproject/controller/AccountController.java
@@ -0,0 +1,20 @@
+package com.example.boardproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/account")
+public class AccountController {
+
+    @GetMapping("/signin")
+    public String signIn() {
+        return "account/signin";
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "account/register";
+    }
+}
