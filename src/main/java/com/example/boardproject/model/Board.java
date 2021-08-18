86eded55a4fe83300bec1057877048d4e3e24543
sjswjeon <sangwon.jeon.uk@gmail.com>
2021-08-18 22:52:29 +0900

    -게시판 데이터베이스 생성 및 연결
    -게시판 목록(list.html) 데이터베이스 연결/출력

---
+++ src/main/java/com/example/boardproject/model/Board.java
@@ -0,0 +1,21 @@
+package com.example.boardproject.model;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+public class Board {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String content;
+    private String date;
+}
---
+++ src/main/java/com/example/boardproject/repository/BoardRepository.java
@@ -0,0 +1,9 @@
+package com.example.boardproject.repository;
+
+import com.example.boardproject.model.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BoardRepository extends JpaRepository<Board, Long> {
+}
