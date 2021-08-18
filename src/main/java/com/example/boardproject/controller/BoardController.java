2f9ff773aeaeb05c8b5933828ff6eb42558a6f1f
sjswjeon <sangwon.jeon.uk@gmail.com>
2021-08-18 23:03:41 +0900

    -게시물 업로드 기능 추가. (날짜는 같이 저장 안됨)
    -form.html 생성

--- src/main/java/com/example/boardproject/controller/BoardController.java
+++ src/main/java/com/example/boardproject/controller/BoardController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -17,10 +19,25 @@ public class BoardController {
     @Autowired
     private BoardRepository boardRepository;
 
+    //    전체 게시물 목록
     @GetMapping("/list")
     public String list(Model model) {
         List<Board> boards = boardRepository.findAll();
         model.addAttribute("boards", boards);
         return "board/list";
+    }
+
+    //    게시물 작성 폼 페이지
+    @GetMapping("/form")
+    public String form(Model model) {
+        model.addAttribute("board", new Board());
+        return "board/form";
+    }
+
+    //    게시물 업로드
+    @PostMapping("/form")
+    public String postForm(@ModelAttribute Board board) {
+        boardRepository.save(board);
+        return "redirect:/board/list";
     }
 }
