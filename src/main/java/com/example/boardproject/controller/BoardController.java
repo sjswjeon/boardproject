ef46525c230e5bb021e5a1edbf2e35134a4e0d48
sjswjeon <sangwon.jeon.uk@gmail.com>
2021-08-18 23:15:35 +0900

    -게시물 업로드 validator 생성
    -validation 에러메세지 출력 (form.html)

--- src/main/java/com/example/boardproject/controller/BoardController.java
+++ src/main/java/com/example/boardproject/controller/BoardController.java
@@ -3,14 +3,17 @@ package com.example.boardproject.controller;
 import com.example.boardproject.model.Board;
 import com.example.boardproject.repository.BoardRepository;
 import com.example.boardproject.service.BoardService;
+import com.example.boardproject.validator.BoardValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -22,6 +25,9 @@ public class BoardController {
 
     @Autowired
     private BoardService boardService;
+
+    @Autowired
+    private BoardValidator boardValidator;
 
     //    전체 게시물 목록
     @GetMapping("/list")
@@ -40,7 +46,11 @@ public class BoardController {
 
     //    게시물 업로드
     @PostMapping("/form")
-    public String postForm(@ModelAttribute Board board) {
+    public String postForm(@Valid Board board, BindingResult bindingResult) {
+        boardValidator.validate(board, bindingResult);
+        if (bindingResult.hasErrors()) {
+            return "board/form";
+        }
         boardService.save(board);
         return "redirect:/board/list";
     }
---
+++ src/main/java/com/example/boardproject/validator/BoardValidator.java
@@ -0,0 +1,26 @@
+package com.example.boardproject.validator;
+
+import com.example.boardproject.model.Board;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+import org.thymeleaf.util.StringUtils;
+
+@Component
+public class BoardValidator implements Validator {
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return Board.class.equals(clazz);
+    }
+
+    @Override
+    public void validate(Object obj, Errors errors) {
+        Board b = (Board) obj;
+        if (StringUtils.isEmpty(b.getTitle())) {
+            errors.rejectValue("title", "key", "Please provide a valid title");
+        }
+        if (StringUtils.isEmpty(b.getContent())) {
+            errors.rejectValue("content", "key", "Please provide a valid content");
+        }
+    }
+}
